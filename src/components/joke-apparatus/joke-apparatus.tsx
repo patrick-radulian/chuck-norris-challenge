491aec6d1022583aa721d7e850265210b36b7d94
Patrick Radulian <patrick.radulian@gmail.com>
2022-02-09 08:28:34 +0100

    Display confirmation message for sent emails

--- src/components/joke-apparatus/joke-apparatus.tsx
+++ src/components/joke-apparatus/joke-apparatus.tsx
@@ -1,6 +1,6 @@
 import { LoadingButton } from "@mui/lab";
 import { Box, Button, Typography } from "@mui/material";
-import { grey, red } from "@mui/material/colors";
+import { green, grey, red } from "@mui/material/colors";
 import CachedIcon from '@mui/icons-material/Cached';
 import SendIcon from '@mui/icons-material/Send';
 import { useGlobalContext } from "context/global-context";
@@ -21,12 +21,16 @@ type SendJokeResponse = {
     invalidEmails: Array<string>
 }
 
+type Status = {
+    message: string,
+    type: "success" | "error"
+}
+
 function JokeApparatus() {
     const { emails, joke, setJoke } = useGlobalContext();
     const [sending, setSending] = React.useState<boolean>(false);
-    const [errorMessage, setErrorMessage] = React.useState<string>("");
-    const [showError, setShowError] = React.useState<boolean>(false);
-    const currentJoke = React.useRef<string>(joke);
+    const [status, setStatus] = React.useState<Status>({message: "", type: "success"});
+    const [showStatus, setShowStatus] = React.useState<boolean>(false);
 
     const fetchNewJoke = React.useCallback(async () => {
         return new Promise<RandomJoke>((resolve, reject) => {
@@ -38,18 +42,15 @@ function JokeApparatus() {
                 })
                 .catch(error => {
                     console.error(error);
-                    setJoke("");
-                    setErrorMessage("Failed to fetch new joke. Please check the console for details.");
-                    setShowError(true);
+                    setStatus({message: "Failed to fetch new joke. Please check the console for details.", type: "error"});
+                    setShowStatus(true);
                 });
         })
-    }, [setJoke]);
+    }, []);
 
     const refreshJoke = React.useCallback(async () => {
         const response = await fetchNewJoke();
-
         setJoke(response.value.joke);
-        currentJoke.current = response.value.joke;
     }, [setJoke, fetchNewJoke]);
 
     const sendMails = React.useCallback(async () => {
@@ -69,41 +70,43 @@ function JokeApparatus() {
 
             if (response.ok) {
                 console.log(`Status: ${status}, Message: ${data.message}, Invalid Emails: ${data.invalidEmails.toString()}`);
+                setStatus({message: data.message, type: "success"});
+                setShowStatus(true);
             } else {
                 console.error(data.message);
-                setJoke("");
-                setErrorMessage(data.message);
-                setShowError(true);
+                setStatus({message: data.message, type: "error"});
+                setShowStatus(true);
             }
         }
 
         setSending(false);
-    }, [joke, emails, setJoke]);
+    }, [joke, emails]);
 
     React.useEffect(() => {
-        if (!showError) return;
+        if (!showStatus) return;
 
         const timeoutID = setTimeout(() => {
-            setShowError(false);
-            setErrorMessage("");
-            setJoke(currentJoke.current);
+            setShowStatus(false);
         }, 5000);
 
         return () => {
             clearTimeout(timeoutID);
         }
-    }, [showError, setJoke]);
+    }, [showStatus]);
 
     React.useEffect(() => {
         refreshJoke();
     }, [refreshJoke]);
 
     return (
-        <Box p={1} sx={{border: 1, borderColor: grey[800], borderRadius: 2}}>
-            <Box sx={{color: red[600]}} className={`${styles.jokeFetchError} ${showError ? styles.animate : ""}`}>
-                <Typography>{errorMessage}</Typography>
-            </Box>
-            <Box p={1}>{joke}</Box>
+        <Box sx={{border: 1, borderColor: grey[800], borderRadius: 2}}>
+            {showStatus ? (
+                <Box p={2} sx={{color: status.type === "error" ? red[600] : green[400]}} className={`${styles.jokeFetchError}`}>
+                    <Typography>{status.message}</Typography>
+                </Box>
+            ) : (
+                <Box p={2}>{joke}</Box>
+            )}
 
             <Box display="flex" justifyContent="flex-end">
                 <Button sx={{mx: 1}} onClick={refreshJoke} startIcon={<CachedIcon/>}>Fetch New Joke</Button>
